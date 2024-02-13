44a0b10c3a595772a7bffbd6fe822b5184a118ca
Javy-Joshua <javyjoshua@gmail.com>
2024-02-13 14:02:52 +0100

    successfully worked on date

--- users/users.controller.js
+++ users/users.controller.js
@@ -1,40 +1,76 @@
 const UserModel = require('../model/user.model')
 const logger = require('../logger/logger')
 const moment = require('moment')
+const { DateTime } = require('luxon')
 
 require('dotenv').config()
 
 const CreateUser = async (req, res) => {
     try {
-        // logger.info("")
-        const userfromRequest = req.body
-        console.log(userfromRequest)
-        const existingUser = await UserModel.findOne({
-            email: userfromRequest.email
-        })
- 
-        if (existingUser) {
-            return res.status(409).json({
-                message: "Details already exist"
-            })
-        }
+      // logger.info("")
+      const userfromRequest = req.body;
+      console.log(userfromRequest);
+      const existingUser = await UserModel.findOne({
+        email: userfromRequest.email,
+      });
 
-        const birthday = new Date(userfromRequest.DOB);
-        // const birthday = moment(userfromRequest.DOB, "MM-DD-YY").toDate();
-        // console.log(birthday)
+      if (existingUser) {
+        return res.status(409).json({
+          message: "Details already exist",
+        });
+      }
 
-        const user = await UserModel.create({
-            name: userfromRequest.name,
-            email: userfromRequest.email,
-            DOB: birthday
-            // DOB: userfromRequest.DOB
-        })
-        console.log(user)
+      // Output the date string to console for debugging
+      console.log("Date of birth string:", userfromRequest.DOB);
 
-        return res.status(201).json({
-            message:"Details successfully created",
-            user,
-        })
+      // Validate the date string format (optional)
+      // Example regular expression to validate MM/dd/yyyy format
+      //   const dateFormatRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+      //   if (!dateFormatRegex.test(userfromRequest.DOB)) {
+      //     return res.status(400).json({
+      //       message: "Invalid date format",
+      //     });
+      //   }
+
+      let dob;
+      try {
+        dob = DateTime.fromFormat(userfromRequest.DOB, "MM-dd-yyyy", {
+          zone: "Africa/Lagos",
+        });
+      } catch (error) {
+        return res.status(400).json({
+          message: "Invalid date format",
+          error: error.message,
+        });
+      }
+
+      if (!dob.isValid) {
+        return res.status(400).json({
+          message: "Invalid date",
+        });
+      }
+
+      // Adjust the date by one day to ensure consistency when saved to the database
+      const adjustedDOB = dob.plus({ days: 1 });
+
+      // Convert the adjusted West Africa DateTime object to UTC
+      const dobUTC = adjustedDOB.toUTC();
+      //   const dobUTC = dob.toUTC();
+      console.log(dob);
+      console.log(dobUTC);
+
+      const user = await UserModel.create({
+        name: userfromRequest.name,
+        email: userfromRequest.email,
+        DOB: dobUTC.toJSDate(),
+        // DOB: userfromRequest.DOB    dobUTC.toJSDate()
+      });
+      console.log(user);
+
+      return res.status(201).json({
+        message: "Details successfully created",
+        user,
+      });
     }
     catch (error) {
         logger.error(error.message)
